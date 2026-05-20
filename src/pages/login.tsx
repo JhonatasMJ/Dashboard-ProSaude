22f10e30faef5aaa85dce5b815035aa7b14d01b9
JhonatasMJ <jhonatasjhmj@gmail.com>
2026-05-20 10:17:44 -0300

    feat/config tailwind and shadcn/ui

---
+++ src/pages/login.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function Login() {
+  return (
+    <div className="bg-red-500">
+      <h1>Login</h1>
+      <Button>Login</Button>
+    </div>
+  )
+}
